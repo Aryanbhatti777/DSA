88995611480fceb22b7d08b7866a5a26019eae4d
Aryan <bhattiaryan777@gmail.com>
2026-03-30 17:38:29 +0530

    done

--- Basics/script.js
+++ Basics/script.js
@@ -1,37 +1,37 @@
-// // print from 1 to n
+// print from 1 to n
 
-// for (let i = 1; i <= 10; i++){
-//     console.log(i);
-// }
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
 
-// // print from n to 1
-// for (let i = 10; i>0; i--){
-//     console.log(i);
-// }
+// print from n to 1
+for (let i = 10; i>0; i--){
+    console.log(i);
+}
 
-// // sum of first n natural numbers
-// let sum1 = 0;
-// for (let i = 1; i<=7; i++){
-//     sum1 += i;
-// }
-// console.log(sum1);
+// sum of first n natural numbers
+let sum1 = 0;
+for (let i = 1; i<=7; i++){
+    sum1 += i;
+}
+console.log(sum1);
 
-// // sum of first n odd numbers
-// let sum2 = 0;
-// for(let i = 1; i<= 10; i++){
-//     if(i%2 !== 0){
-//         sum2 += i;
-//     }
-// }
-// console.log(sum2)
+// sum of first n odd numbers
+let sum2 = 0;
+for(let i = 1; i<= 10; i++){
+    if(i%2 !== 0){
+        sum2 += i;
+    }
+}
+console.log(sum2)
 
 
-// // count the numbers
-// let count = 0;
-// for(let i = 1; i<= 100; i++){
-//     count += 1;
-// }
-// console.log(count);
+// count the numbers
+let count = 0;
+for(let i = 1; i<= 100; i++){
+    count += 1;
+}
+console.log(count);
 
 // count even numbers 
 
