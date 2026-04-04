88b8bddbe0d01f70ed513f433c1c760fa3446888
Aryan <bhattiaryan777@gmail.com>
2026-04-04 18:40:40 +0530

    done|

--- Basics/script.js
+++ Basics/script.js
@@ -311,3 +311,14 @@ for(let i = 1; i<= 5; i++){
     }
     console.log(row)
 }
+
+
+// printing reverse stars
+
+for(let i = 1; i<=5; i++){
+    let row = "";
+    for(let j = 1; j <= 5-i+1; j++){
+        row += "*"
+    }
+    console.log(row)
+}
