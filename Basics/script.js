8cbb710e5e7f68d770c3e9aa584389852f925cc0
Aryan <bhattiaryan777@gmail.com>
2026-06-07 11:24:04 +0530

    duplicate found

--- Basics/script.js
+++ Basics/script.js
@@ -548,3 +548,15 @@ while(i !== 0){
 }
 
 console.log(arr)
+
+let arr = [1, 2, 3, 2, 4, 1];
+let digits = [];
+
+for(let i = 0; i<arr.length; i++){
+    for(let j = i+1; j<arr.length; j++){
+        if(arr[i] === arr[j]){
+            digits.push(arr[i])
+        }
+    }
+}
+digits.forEach(item => console.log(item))
